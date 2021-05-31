5da172a1e1f8a2f5213321529ffe1e6c67d89507
SanketDhabarde <sanketdhabarde13@gmail.com>
2021-05-31 14:50:19 +0530

    refactor

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect} from "react";
 import ReactQuill from 'react-quill';
-import debounce from '../../helper';
 import { withStyles } from '@material-ui/core/styles';
 import Style from './Style';
 import { db } from '../../firebase';
@@ -11,8 +10,6 @@ const Editor = ({classes, selectedNote}) => {
     const [text, setText] = useState('');
     const [title, setTitle] = useState('');
     const [id, setId] = useState('');
-    const updateBodyDebounce = debounce(text, 1500);
-    const updateTitleDebounce = debounce(title, 1500);
     
     useEffect(() => {
         setText(selectedNote.body);
@@ -20,27 +17,35 @@ const Editor = ({classes, selectedNote}) => {
         setId(selectedNote.id);
     },[selectedNote.id, selectedNote.title, selectedNote.body])
     
+    // use debaunce effect to update the body
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        db.collection('notes')
+        .doc(selectedNote.id)
+        .update({
+          body: text,
+        });
+      }, 1500);
+
+      return () => clearTimeout(timer);
+
+    }, [selectedNote.id, text]);
 
     useEffect(() => {
-        if (updateBodyDebounce) {
-          db
-            .collection('notes')
-            .doc(selectedNote.id)
-            .update({
-              body: text,
-            });
-        }
-      }, [updateBodyDebounce, selectedNote.id, text]);
-    
-      useEffect(() => {
-          db
-          .collection('notes')
-          .doc(selectedNote.id)
-          .update({
-            title: title,
-          });
-      }, [updateTitleDebounce, selectedNote.id, title]);
+      const timer = setTimeout(() => {
 
+        db.collection('notes')
+        .doc(selectedNote.id)
+        .update({
+          title: title,
+        });
+
+      }, 1500);
+
+      return () => clearTimeout(timer);
+
+    }, [selectedNote.id, title])
+    
 
     const updateBody = async (val) =>{
         await setText(val);
--- src/helper.js
+++ src/helper.js
@@ -1,20 +1,3 @@
-import { useEffect, useState } from "react";
-
-export default function useDebounce(value, delay) {
-  const [debounceValue, setDebounceValue] = useState(value);
-
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebounceValue(value);
-    }, delay);
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [value, delay]);
-
-  return debounceValue;
-}
-  
 export function removeHTMLTags (str) {
     return str.replace(/<[^>]*>?/gm, '');
 };
